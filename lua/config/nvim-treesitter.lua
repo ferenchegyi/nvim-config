71e5a8bc5f9bec1b704a68b94297775c0d1e4236
Ferenc Hegyi <hegyiferenc@proton.me>
2025-07-14 19:05:29 +0200

    Add nvim-treesitter to the config

---
+++ lua/config/nvim-treesitter.lua
@@ -0,0 +1,12 @@
+require('nvim-treesitter').setup({
+  install_dir = vim.fn.stdpath('data') .. '/site'
+})
+
+require("nvim-treesitter").install({
+  "rust", "javascript", "typescript", "tsx", "jsx", "lua",
+  "css", "html", "scss", "json", "yaml", "toml",
+  "sql", "markdown", "markdown_inline", "dockerfile", "bash", "vim"
+})
+
+require("nvim-treesitter.highlighter").attach()
+require("nvim-treesitter.indent").attach()
