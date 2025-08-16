71a41d83ca933a784377dbf2e6738aeee464df04
Ferenc Hegyi <hegyiferenc@proton.me>
2025-08-16 19:44:53 +0200

    Add JavaScript LSP support

--- lsp/typescriptls.lua
+++ lsp/typescriptls.lua
@@ -1,6 +1,6 @@
 return {
   cmd = { 'typescript-language-server', '--stdio' },
-  filetypes = { 'typescript', 'typescriptreact' },  -- Corresponds to .ts and .tsx files
+  filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
   root_markers = { 'tsconfig.json', 'package.json' },
   settings = {
     tsserver = {
